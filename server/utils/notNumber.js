a2233d2948a8433267c0fb8d923ef15a800102ba
EugeniaArnaud <euge.arnaud@gmail.com>
2022-07-25 05:07:17 -0300

    id

--- server/utils/notNumber.js
+++ server/utils/notNumber.js
@@ -1,5 +1,5 @@
 const notNumber = (id, next) => {
-    if (isNaN(Number(id)) || Number(id) < 1) {
+    if (isNaN(Number(id)) || Number(id) < 0) {
         let error = new Error("ID must be a positive integer")
         error.status = 400
         next(error)
