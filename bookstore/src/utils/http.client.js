d53ad9309327b3956f0fe317d49310de9fbcd8da
EugeniaArnaud <euge.arnaud@gmail.com>
2022-07-25 04:20:26 -0300

    start

--- bookstore/src/utils/http.client.js
+++ bookstore/src/utils/http.client.js
@@ -1,8 +1,8 @@
 import axios from "axios";
-const baseURL = "https://utn-books.herokuapp.com/api"; 
+const baseURL = "https://utn-books.herokuapp.com"; 
 
 const axiosClient = axios.create({
-  baseURL: "https://utn-books.herokuapp.com/api", 
+  baseURL: "https://utn-books.herokuapp.com", 
   headers: {
     "Content-Type": "application/json",
   } 
