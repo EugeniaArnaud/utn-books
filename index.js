88ce520e09173c4c7933b995dba58e8073199b93
EugeniaArnaud <euge.arnaud@gmail.com>
2022-07-24 21:10:01 -0300

    commit

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'server/storage')))
 // })
 
 
-app.use(express.static(path.join(__dirname, "./bookstore/build")))
+app.use(express.static(path.resolve(__dirname, "./bookstore/build")))
 
 
 // app.get("/api", (req, res) => {
